c881e2bf9a1e6884cd8be514d442be92c0c34f71
José Roberto Torres Mancilla <j.roberto-torres@hotmail.com>
2014-12-04 06:06:03 -0600

    GUI test 1
    Test 1

--- public/javascripts/dots_and_boxes.js
+++ public/javascripts/dots_and_boxes.js
@@ -1,4 +1,4 @@
-/*
+﻿/*
  Dots and Boxes distributed game.
  Web client.
  Copyright (C) 2014 by José Roberto Torres and Mauricio Cunillé Blando.
@@ -23,151 +23,237 @@ var GAME_ROOT = '/dotsandboxes/';
 var PAUSE = 1000;
 
 //------------------------------------------------------------------------------
-$(document).ready(function () {
+$(document).ready(function() {
+    eraseErrorMessage();
+    $('#new_game_button').click(displayNewGameForm);
+    $('#create_game_button').click(createGame);
+    $('#main_menu_button').click(showMainMenu);
 
-//----------------------------------------------------------------------------
- function createGame() {
+    //----------------------------------------------------------------------------
+    function createGame() {
+        eraseErrorMessage();
 
-    var name = $('#game_name').val().trim();
-    var size = $('#game_size').val().trim();
-    var players = $('#players_num').val().trim();
-    var player_symbol = $('#player_symbol').val().trim();
-    
-    var isValid = true;
+        var name = $('#game_name').val().trim();
+        var size = $('#game_size').val().trim();
+        var players = $('#players_num').val().trim();
+        var player_symbol = $('#player_symbol').val().trim();
 
-    if (name === '') {
-      isValid = false;
-      errorMessage('El nombre del juego no puede quedar vacío.');
-    }
-    
-    if (size === '') {
-      isValid = false;
-      errorMessage('Especifique un tamaño de juego válido.');
-    }
-    
-    if (players === '') {
-      isValid = false;
-      errorMessage('Introdusca un número válido de jugadores. De 2 - 4.');
-    }
-    
-    if (player_symbol === '') {
-      isValid = false;
-      errorMessage('Especifique su símbolo de juego.');
-    }
-    
-    if(isValid) {
-      $.ajax({
-        url: GAME_ROOT + 'create_game/',
-        type: 'POST',
-        dataType: 'json',
-        data: {
-          name: name,
-          size: size,
-          players: players,
-          player_symbol: player_symbol
-        },
-        error: conexionError,
-        success: function(result) {
-          var text;
-          if (result.created) {
-            $('div').hide();
-            $('#simbolo').html(result.symbol);
-            $('#mensaje_1').html('Esperando a que alguien más se una al ' +
-              'juego <strong>' + scapeHtml(name) + '</strong>.');
-            $('#boton_mensajes_regresar_al_menu').hide();
-            $('#seccion_mensajes').show();
-            $('#seccion_tablero').show();
-            waitTurn();
-          } else {
-            switch (result.code) {
+        var isValid = true;
 
-            case 'duplicate':
-              text = 'Alguien más ya creó un juego con este ' +
+        if(name === '') {
+            isValid = false;
+            errorMessage('El nombre del juego no puede quedar vacío.');
+        }
+
+        if(size === '') {
+            isValid = false;
+            errorMessage('Especifique un tamaño de juego válido.');
+        }
+
+        if(players === '') {
+            isValid = false;
+            errorMessage('Introdusca un número válido de jugadores. De 2 - 4.');
+        }
+
+        if(player_symbol === '') {
+            isValid = false;
+            errorMessage('Especifique su símbolo de juego.');
+        }
+
+        if(isValid) {
+
+            $.ajax({
+                url: GAME_ROOT + 'create_game/',
+                type: 'POST',
+                dataType: 'json',
+                data: {
+                    name: name,
+                    size: size,
+                    players: players,
+                    player_symbol: player_symbol
+                },
+                error: conexionError,
+                success: function(result) {
+                    var text;
+                    if(result.created) {
+                        errorMessage('Juego creado')
+                    } else {
+                        switch(result.code) {
+
+                            case 'duplicate':
+                                text = 'Alguien más ya creó un juego con este ' +
                 'nombre: <em>' + scapeHtml(name) + '</em>';
-              break;
+                                break;
 
-            case 'invalid':
-              text = 'No se proporcionó un nombre de juego válido.';
-              break;
+                            case 'invalid':
+                                text = 'No se proporcionó un nombre de juego válido.';
+                                break;
 
-            default:
-              text = 'Error desconocido.';
-              break;
-            }
-            errorMessage(text);
-          }
+                            default:
+                                text = 'Error desconocido.';
+                                break;
+                        }
+                        errorMessage(text);
+                    }
+                }
+            });
         }
-      });
+        return false; // Se requiere para evitar que la forma haga un "submit".
     }
-    return false; // Se requiere para evitar que la forma haga un "submit".
-  }
-  
-  //----------------------------------------------------------------------------
-  function errorMessage(message) {
-    $('body').css('cursor', 'auto');
-    $('div').hide();
-    $('#error_message').html(message);
-    $('#error_section').show();
-  }
-  
-  //----------------------------------------------------------------------------
-  // Para evitar inyecciones de HTML.
-  function scapeHtml (str) {
-    return $('<div/>').text(str).html();
-  }
-  
-  //----------------------------------------------------------------------------
-  function waitTurn() {
 
-    var secs = 0;
+    //------------------------------------------------------------------------------
 
-    $('body').css('cursor', 'wait');
+    $('#get_games_button').click(function() {
+        $.ajax({
+            url: GAME_ROOT + 'available_games/',
+            type: 'GET',
+            dataType: 'json',
+            error: conexionError,
+            success: function(result) {
 
-    function ticToc() {
-      $('#message_3').html('Llevas ' + secs + ' segundo' +
-        (secs === 1 ? '' : 's') + ' esperando.');
-      secs++;
-      $.ajax({
-        url: GAME_ROOT + 'state/',
-        type: 'GET',
-        dataType: 'json',
-        error: conexionError,
-        success: function(result) {
+                if(result.length === 0) {
+                    errorMessage("No hay juegos disponibles");
+                } else {
+                    var gList = result.map(function(x) {
+                        return '<li value="' + x._id + '" class="game">' + scapeHtml(x.name) + '</li>'
+                    });
 
-          switch (result.state) {
+                    $('#games_list > ul').html(gList.join(''));
 
-          case 'tu_turno':
-            turnoTirar(result.board);
-            break;
+                    $('.game').click(joinGame);
 
-          case 'wait':
-            setTimeout(ticToc, PAUSE);
-            break;
+                    $('#main_menu').fadeOut("slow", function() {
+                        $('#main_menu_button').fadeIn("slow", function() {
+                            $('#games_list').fadeIn("slow");
+                        });
+                    });
+                }
+            }
+        });
+    });
 
-          case 'tie':
-            actualizar(result.board);
-            finDeJuego('<strong>Empate.</strong>');
-            break;
+    //----------------------------------------------------------------------------
+    function errorMessage(message) {
+        $('body').css('cursor', 'auto');
+        $('#error_message').html(message);
+        $('#error_message').fadeIn("slow");
+    }
 
-          case 'win':
-            finDeJuego('<strong>Ganaste.</strong> ¡Felicidades!');
-            resalta(result.board);
-            break;
+    //----------------------------------------------------------------------------
+    // Para evitar inyecciones de HTML.
+    function scapeHtml(str) {
+        return $('<div/>').text(str).html();
+    }
 
-          case 'lost':
-            finDeJuego('<strong>Perdiste.</strong> ¡Lástima!');
-            actualizar(result.board);
-            resalta(result.board);
-            break;
-          }
-        }
-      });
+    //----------------------------------------------------------------------------
+    function waitTurn() {
+        eraseErrorMessage();
+
+        var secs = 0;
+
+        $('body').css('cursor', 'wait');
+
+        function ticToc() {
+            errorMessage('Llevas ' + secs + ' segundo' + (secs === 1 ? '' : 's') + ' esperando.');
+            secs++;
+            $.ajax({
+                url: GAME_ROOT + 'state/',
+                type: 'GET',
+                dataType: 'json',
+                error: conexionError,
+                success: function(result) {
+
+                    switch(result.state) {
+
+                        case 'your_turn':
+                            turnoTirar(result.board);
+                            break;
+
+                        case 'wait':
+                            setTimeout(ticToc, PAUSE);
+                            break;
+
+                        case 'tie':
+                            actualizar(result.board);
+                            finDeJuego('<strong>Empate.</strong>');
+                            break;
+
+                        case 'win':
+                            finDeJuego('<strong>Ganaste.</strong> ¡Felicidades!');
+                            resalta(result.board);
+                            break;
+
+                        case 'lost':
+                            finDeJuego('<strong>Perdiste.</strong> ¡Lástima!');
+                            actualizar(result.board);
+                            resalta(result.board);
+                            break;
+                    }
+                }
+            });
+        };
+        setTimeout(ticToc, 0);
     };
-    setTimeout(ticToc, 0);
-  };
-  
-  //----------------------------------------------------------------------------
-  function conexionError() {
-    errorMessage('No es posible conectarse al servidor.');
-  }
+
+    //----------------------------------------------------------------------------
+    function conexionError() {
+        errorMessage('No es posible conectarse al servidor.');
+    }
+
+    //----------------------------------------------------------------------------
+    function joinGame() {
+        eraseErrorMessage();
+
+        var id = $(this).val();
+        var symbol;
+
+        $.ajax({
+            url: GAME_ROOT + 'join_game/',
+            type: 'PUT',
+            dataType: 'json',
+            data: { id_game: id, symbol: symbol },
+            error: conexionError,
+            success: function(result) {
+                if(result.joined && result.code === 'ok') {
+                    errorMessage('Por favor espera tu turno.');
+                }
+                else {
+                    switch(result.code) {
+                        case 'bad_id':
+                            errorMessage('Error al unirse al juego: ' + name);
+                            break;
+                        case 'used_symbol':
+                            errorMessage('El símbolo: ' + symbol + ' ya está en uso.');
+                            break;
+                        default:
+                            errorMessage('No es posible unirse al juego, inténtelo más tarde.');
+                    }
+                }
+            }
+        });
+    }
+
+    //----------------------------------------------------------------------------
+    function displayNewGameForm() {
+        eraseErrorMessage();
+        $('#main_menu').fadeOut("slow", function() {
+            $('#main_menu_button').fadeIn("slow", function() {
+                $('#new_game').fadeIn("slow");
+            });
+        });
+    }
+
+    function showMainMenu() {
+        eraseErrorMessage();
+        $('#new_game').fadeOut("slow", function() {
+            $('#main_menu_button').fadeOut("slow", function() {
+                $('#main_menu').fadeIn("slow");
+            });
+        });
+    }
+
+    function eraseErrorMessage() {
+        $('#error_message').fadeOut("slow");
+        $('#error_message').html("");
+    }
 });
